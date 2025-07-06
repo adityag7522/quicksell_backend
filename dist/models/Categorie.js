31b8e525b9c0df22513289e253de9e1f0137ae36
Aditya <adityag7522@gmail.com>
2025-07-06 18:49:48 +0530

    aded vercel

--- dist/models/Categorie.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CategorySchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  count: Number,
-});
-
-module.exports = mongoose.model('Categories', CategorySchema);
